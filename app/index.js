0a1a13f0d0a7c541b9e5c20d4cb52f07e57bb417
santoshpatil96 <35653819+patilsp@users.noreply.github.com>
2023-05-18 11:56:01 +0530

    fetching api data

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
-import { Text } from "react-native";
+import { Redirect } from "expo-router";
 
-export default function Home() {
-    return <Text > Home page < /Text>;
+export default function Index() {
+    return <Redirect href = "/home" / > ;
 }
